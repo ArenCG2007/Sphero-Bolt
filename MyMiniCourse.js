98584c87bf3e877904ee988da4fbceb960525f9e
Gay <arencg2007@gmail.com>
2024-04-23 13:35:50 -0400

    Initial MyMiniCourse.Js Commit Part 2

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+//Programmer: Aren Gay\
+//Team Member: Jordan Gibbs\
+//Bolt ID:B0D0
+//Date: 4.23.24
+//Program: My Mini Course
+//Program URL:  https://edu.sphero.com/program/16743341/edit
+
+async function startProgram() {
+	// Write code here
+}
