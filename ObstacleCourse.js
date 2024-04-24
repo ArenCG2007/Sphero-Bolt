f34ed73304473008f072d1085198f6a9c01cbd7c
Gay <arencg2007@gmail.com>
2024-04-24 14:00:55 -0400

    Check Points 1&2 Commit

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -4,3 +4,14 @@
 //Date: 4.24.24
 //Program: Obstacle Course
 //Program URL:  https://edu.sphero.com/program/16752513/edit
+
+async function startProgram() {
+	await roll((getHeading() + 0), 60, 1.7);
+	await delay(1);
+
+	await roll((getHeading() + 90), 10, 2);
+	await delay(1);
+
+	await roll((getHeading() + 0), 50,1.4);
+	await delay(1);
+}
