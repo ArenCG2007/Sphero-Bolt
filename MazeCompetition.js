660838e3ae17949e32da110c96540b3134d60edc
Gay <arencg2007@gmail.com>
2024-04-30 13:26:39 -0400

    Initial fixes and URL setup

---
+++ MazeCompetition.js
@@ -0,0 +1,9 @@
+//Programmer: Aren Gay\
+//Team Member: Jordan Gibbs\
+//Bolt ID:B0D0
+//Date: 4.30.24
+//Program: Maze Competition
+//Program URL:https://edu.sphero.com/program/16788338/edit
+async function startProgram() {
+	// Write code here
+}
